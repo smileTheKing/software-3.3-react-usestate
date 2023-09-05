2a6b3dbf9506c9ee0f37c31bb0cbe127619db8be
smiletheking <s.cammue@gmail.com>
2023-09-04 19:02:42 -0500

    final code

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
 
-const  Counter =() =>{
+const Counter = () => {
+   //useState Hook
   const [count, setCount] = useState(0);
 
+
+   //countPlus function
   const handlerPlus = () => {
-    setCount(count + 1);
+    setCount((prev)=>prev+1);
   };
+
+
+  //countMinus function
   const handlerMinus = () => {
-    setCount(count - 1);
+    setCount((prev)=>{
+      if(prev === 0){
+        return prev;
+      }else{
+        return prev - 1;
+      }
+
+    
+    });
+  };
+//reset count function
+  const handlerReset = () => {
+    setCount(0);
   };
 
   return (
-    <>
-      <button onClick={handlerMinus}>-</button>
-      <span>{count}</span>
-      <button onClick={handlerPlus}>+</button>
-    </>
+    <div className="container">
+      <div className="wrapper">
+        <div className="btn-container">
+          <button onClick={handlerMinus} className="warning">-</button>
+          <span className="count">{count}</span>
+          <button onClick={handlerPlus} className="primary">+</button>
+        </div>
+
+        <button onClick={handlerReset} className="danger">reset</button>
+      </div>
+    </div>
   );
-}
+};
 export default Counter;
